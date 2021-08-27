fdce4564d74c9110a38f03e64daa0e9ba9cf05a5
dmitriy <yulin_dimon@mail.ru>
2021-08-27 15:56:45 +0300

    I made the models function universal and added setTimeout to the models function to pop up the modal window after 60 seconds

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -1,33 +1,50 @@
 const modals = () => {
-    function bindModal(trigger, modal, close) {
-        trigger.addEventListener('click', (e) => {
-            if (e.target) {
-                e.preventDefault();
-            }
+    function bindModal(triggerSelector, modalSelector, closeSelector) {
 
-            modal.style.display = 'block';
-            document.body.style.overflow = 'hidden';
-        });
+        const trigger = document.querySelectorAll(triggerSelector)
+        const modal = document.querySelector(modalSelector)
+        const close = document.querySelector(closeSelector)
+
+        trigger.forEach((item) => {
+            item.addEventListener('click', (e) => {
+                if (e.target) {
+                    e.preventDefault();
+                }
+
+                modal.style.display = 'block';
+                document.body.style.overflow = 'hidden';
+                // document.body.classList.add('modal-open')
+            });
+        })
 
         close.addEventListener('click', () => {
             modal.style.display = 'none';
             document.body.style.overflow = '';
+            document.body.classList.remove('modal-open')
         });
 
-        modal.addEventListener('click',(e)=>{
-            if(e.target === modal){
+        modal.addEventListener('click', (e) => {
+            if (e.target === modal) {
                 modal.style.display = 'none';
                 document.body.style.overflow = '';
+                // document.body.classList.remove('modal-open')
             }
         });
     }
 
-    const callEngineerBtn = document.querySelector('.popup_engineer_btn')
-    const modalEngineer = document.querySelector('.popup_engineer')
-    const modalEngineerClose = document.querySelector('.popup_engineer .popup_close')
+    function showModalByTime(selector, time){
+        setTimeout(()=>{
+            document.querySelector(selector).style.display = 'block'
+            document.body.style.overflow = 'hidden';
+        },time)
+    }
 
-    bindModal(callEngineerBtn,modalEngineer,modalEngineerClose)
+
+    bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
+    bindModal('.phone_link','.popup', '.popup .popup_close');
+    showModalByTime('.popup', 30000)
 };
+
 
 
 export default modals;
