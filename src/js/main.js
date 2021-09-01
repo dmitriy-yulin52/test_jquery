ee39a0a5bb3ee5300a6cd33a3eef3b861d8bfde2
dmitriy <yulin_dimon@mail.ru>
2021-09-01 21:58:15 +0300

    Made the changeModalState file

--- src/js/main.js
+++ src/js/main.js
@@ -5,11 +5,12 @@ import forms from './modules/forms'
 
 
 window.addEventListener('DOMContentLoaded',()=>{
-    "use strict"
+    let modalState = {}
+
     modals();
-    tabs('.glazing_slider','.glazing_block','.glazing_content','active');
-    tabs('.decoration_slider','.no_click','.decoration_content > div > div','after_click');
-    tabs('.balcon_icons','.balcon_icons_img','.big_img > img','do_image_more', 'inline-block');
-    forms();
+    tabs('.glazing_slider ', '.glazing_block', '.glazing_content', 'active');
+    tabs('.decoration_slider', '.no_click', '.decoration_content > div > div', 'after_click');
+    tabs('.balcon_icons', '.balcon_icons_img', '.big_img > img', 'do_image_more', 'inline-block');
+    forms(modalState);
 })
 
---
+++ src/js/modules/changeModalState.js
@@ -0,0 +1,10 @@
+
+const changeModalState = (state)=> {
+    const windowForm = document.querySelectorAll('.balcon_icons_img')
+    const windowWidth = document.querySelector('#width')
+    const windowHeight = document.querySelector('#height')
+    const windowType = document.querySelector('#view_type')
+    const windowProfile = document.querySelectorAll('#view_type')
+}
+
+export default changeModalState
