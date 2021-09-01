10ab355d7ffcecefab67ff68f5285b1a437c6cd2
dmitriy <yulin_dimon@mail.ru>
2021-09-01 23:13:19 +0300

    I made a switch case in the changeModalState file

--- src/js/modules/changeModalState.js
+++ src/js/modules/changeModalState.js
@@ -1,25 +1,48 @@
 import checkNumInputs from './checkNumInputs'
 
-const changeModalState = (state)=> {
+const changeModalState = (state) => {
     const windowForm = document.querySelectorAll('.balcon_icons_img')
-    const windowWidth = document.querySelector('#width')
-    const windowHeight = document.querySelector('#height')
-    const windowType = document.querySelector('#view_type')
+    const windowWidth = document.querySelectorAll('#width')
+    const windowHeight = document.querySelectorAll('#height')
+    const windowType = document.querySelectorAll('#view_type')
     const windowProfile = document.querySelectorAll('.checkbox')
 
     checkNumInputs('#width')
     checkNumInputs('#height')
 
-    function bindActionToElems(event,elem,prop){
-        elem.forEach((item, i)=>{
-            item.addEventListener(event,()=>{
-                state[prop] = i
-                console.log(state)
+    function bindActionToElems(event, elem, prop) {
+        elem.forEach((item, i) => {
+            item.addEventListener(event, () => {
+                switch (item.nodeName) {
+                    case 'SPAN':
+                        console.log('hello')
+                        break
+                    case 'INPUT':
+                        if (item.getAttribute('type') === 'checkbox') {
+                            console.log('checkbox')
+                        }else {
+                            console.log('input')
+                        }
+                        break
+                    case 'SELECT':
+                        console.log('select')
+                        break
+                }
+                // if (elem.length > 1) {
+                //     state[prop] = i
+                // } else {
+                //     state[prop] = item.value
+                // }
+                // console.log(state)
             })
         })
     }
 
-    bindActionToElems('click',windowForm,'form')
+    bindActionToElems('click', windowForm, 'form')
+    bindActionToElems('input', windowHeight, 'height')
+    bindActionToElems('input', windowWidth, 'width')
+    bindActionToElems('change', windowType, 'type')
+    bindActionToElems('change', windowProfile, 'profile')
 }
 
 export default changeModalState
